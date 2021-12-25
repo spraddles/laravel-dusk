eec8d76036dcb333e53623a66a78444577000e8b
brettspradbury <brett.spradbury@gmail.com>
2021-12-26 00:20:50 +1100

    Update ExampleTest.php

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
-use Symfony\Component\DomCrawler\Crawler;
 
 class ExampleTest extends DuskTestCase
 {
@@ -142,7 +141,12 @@ class ExampleTest extends DuskTestCase
             $coinOption = '[data-dialog-name="Symbol Search"] div[class^="listContainer-"]  div[class^="itemRow-"]:nth-of-type(2)';
             $cookieButton = '[data-role="toast-container"] div[class^="toast-wrapper-"] div[class^="actions-"] button';
 
+            // clear strategy
+            $arrowOption = '#drawing-toolbar div[class^="content-"] div[class^="inner-"] div[class^="group-"]:nth-of-type(4) [data-name="removeAllDrawingTools"] div[class^="arrow-"]';
+            $removeAll = '#overlap-manager-root div[class^="menuBox-"] [data-name="remove-all"]';
+
             // pinescript
+            $pineScriptTab = '#footer-chart-panel div[class^="tabs-"] div[class^="tab-"]:nth-of-type(3)';
             $openScriptMenu = '#bottom-area .bottom-widgetbar-content.scripteditor.tv-script-widget div[class^="rightControlsBlock-"] div[data-name="open-script"]';
             $openMyScript = '#overlap-manager-root div[class^="menuBox-"] div[class^="item-"]:first-of-type';
             $strategySearchInput = '#overlap-manager-root div[class^="container-"]:nth-of-type(2) div[class^="inputContainer-"] input';
@@ -151,14 +155,39 @@ class ExampleTest extends DuskTestCase
             $closeStrategySearch = 'div[data-outside-boundary-for="open-user-script-dialog"] div[class^="wrapper-"] div[class^="container-"]:first-of-type span[class^="close-"]';
             $addToChart = '#bottom-area .bottom-widgetbar-content.scripteditor.tv-script-widget #tv-script-pine-editor-header-root div[class^="content-"] div[class^="rightControlsBlock-"] div[data-name="add-script-to-chart"]';
 
+            // date range chart setting
+            $chartSettings = '#bottom-area .bottom-widgetbar-content.backtesting .group:nth-of-type(2) .js-backtesting-open-format-dialog';
+            $inputsTab = '#overlap-manager-root div[data-outside-boundary-for="indicator-properties-dialog"] div[class^="scrollWrap-"] div[class^="tabs-"] [data-value="inputs"]';
+            $selectArrow = '#overlap-manager-root div[data-outside-boundary-for="indicator-properties-dialog"] div[class^="scrollable-"] div[class^="content-"] div[class^="cell-"]:last-of-type span[class^="container-"] span[class^="inner-slot-"]:nth-of-type(2)';
+            $dateRange_1 = 'div[id$="_item_All-2021"]';
+            $okButton = '#overlap-manager-root div[data-outside-boundary-for="indicator-properties-dialog"] div[class^="footer-"] div[class^="buttons-"] span[class^="submitButton-"] button';
+
+            // time interval
+            $intervalMenu = '#header-toolbar-intervals div[class^="menu-"]';
+            $interval = '#overlap-manager-root [data-value="1D"]'; // 1M,1W,1D,240,180,120,60,45,30,15,5
+
             // data points
+            $netProfit = '#bottom-area > div.bottom-widgetbar-content.backtesting > div.backtesting-content-wrapper > div > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div:nth-child(2) > span';
+            //B+H
+            //Difference
+            //Total Trades closed
+            //Trades p/day[calculated]
+            //Trades open
+            //Winning trades
+            //Losing trades
+            //Percent profitable
+            //Win loss ratio
+
+            // CSV file
+            $output = array(
+                $csvHeaders
+            );
+            $csvHeaders = array('Coin,Exchange,Date range,Net profit,B+H,Difference,Total Trades closed,Trades p/day[calculated],Trades open,Winning trades,Losing trades,Percent profitable,Win loss ratio');
 
 
 
 
-
-
-            $html = $browser
+            $browser
 
                 // sign in
                 ->visit( $website )
@@ -173,9 +202,10 @@ class ExampleTest extends DuskTestCase
                 ->press( $focusPassword )
                 ->type( $focusPassword, $password )
                 ->press( $signInButton )
+                ->pause(1000)
                 ->waitFor( $chartLink )
                 ->assertVisible( $chartLink )
-                ->click( $chartLink )
+                ->press( $chartLink )
 
                 // choose exchange
                 ->waitFor( $coinList )
@@ -191,7 +221,12 @@ class ExampleTest extends DuskTestCase
                 ->press( $coinOption )
                 ->press( $cookieButton )
 
+                // clear strategy
+                ->press( $arrowOption )
+                ->press( $removeAll )             
+
                 // open strategy
+                ->press( $pineScriptTab )
                 ->waitFor( $openScriptMenu )
                 ->assertVisible( $openScriptMenu )
                 ->press( $openScriptMenu )
@@ -203,29 +238,34 @@ class ExampleTest extends DuskTestCase
                 ->waitFor( $addToChart )
                 ->press( $addToChart )
 
+                // change date range
+                ->waitFor( $chartSettings )
+                ->press( $chartSettings )
+                ->waitFor( $inputsTab )
+                ->press( $inputsTab )
+                ->waitFor( $selectArrow )
+                ->press( $selectArrow )
+                ->waitFor( $dateRange_1 )
+                ->press( $dateRange_1 )
+                ->pause(1000)
+                ->waitFor( $okButton )
+                ->press( $okButton )
 
-                ->pause(25000);
+                // change time interval
+                ->waitFor( $intervalMenu )
+                ->press( $intervalMenu )
+                ->waitFor( $interval )
+                ->press( $interval )
 
+                ->pause(1000)
 
+                // data points
+                ->waitFor( $netProfit );
+                $data = strstr($browser->text( $netProfit ), ' %', true );
+                echo $data;
 
-                // get data points
-                $crawler = new Crawler($website);
-                $data = $crawler->filter('div[data-react-class="GamePageHeader"]')->attr('data-react-props');
+                // dump into CSV
 
-
-
-
-
-                
-
-
-
-                //->pause(25000);
-
-
-                
-
-                
 
         });
     }
